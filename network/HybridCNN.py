2c37109d2a5be9519730a62f284fb6f07c2e1c13
Wangzeyu <wangzeyu1994@yahoo.com>
2019-07-11 16:25:58 +0900

    update CNN-RNN model, add train.ipynb

--- network/HybridCNN.py
+++ network/HybridCNN.py
@@ -1,10 +1,13 @@
 import torch.nn as nn
+import torch
 
 class TextEncoder(nn.Module):
-    def __init__(self, alphasize, emb_dim, cnn_dim = 256, dropout = 0):
-
+    def __init__(self, alphasize, cnn_dim = 256, dropout = 0):
+        super(TextEncoder, self).__init__()
         self.cnn_net = nn.Sequential(
         # 201 x alphasize(71)
+        # the input should be (N, C_in, L) = [#batch, 71, 201]
+        # which in torch7 the input is (N, L, C_in) -> (N, L, C_out)
         nn.Conv1d(alphasize, 384, 4), # (201-4+1, 384) = (198, 384)
         nn.ReLU(),
         nn.MaxPool1d(3, 3), # ((198-3)/3+1, 384) = (68, 384)
@@ -19,11 +22,13 @@ class TextEncoder(nn.Module):
         # 8 x 256
         )
 
-        self.rnn_net = nn.Sequential(
+        # put #batch x 8 x 256 sequence into an rnn model
+        # two way to get hidden layer's outputs
+        #   1. customize an rnn
+        #   2. try to get the intermidite value of rnn
 
-
-        )
-
+        # length, batch size,
+        self.rnn_net = nn.RNNCell(cnn_dim, cnn_dim, nonlinearity = 'relu')
 
 
     def forward(self, input):
@@ -32,4 +37,21 @@ class TextEncoder(nn.Module):
             input (array): onehot text vector, 201 * 71, [doc_length] * [alphabet_size]
         """
         output = self.cnn_net(input)
-        # return #batch x 8 x 256
+        n_batch, n_channel, length = output.shape
+
+        # change the shape of in
+        output = output.view([length, n_batch, n_channel])
+        h0 = torch.zeros(n_batch, n_channel)
+        input0 = torch.ones(n_batch, n_channel)
+        h = self.rnn_net(input0, h0)
+
+        sum_hiden_layers = h0
+        for i in range(length):
+            h = self.rnn_net(output[i], h)
+            sum_hiden_layers += h
+
+        return sum_hiden_layers / length
+        # return #batch x 256, the encoder
+
+if __name__ == '__main__':
+    import birds_loader
--- train.py
+++ train.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 import os
-
+import torch
+from network import HybridCNN
 from data_loader.birds_loader import BirdsDataLoader
 
 data_root = '/Users/rhonin/Desktop/Github/Learning_Deep_Representations/cvpr2016_cub'
@@ -9,13 +10,21 @@ image_dir = os.path.join(data_root, 'images')
 text_dir = os.path.join(data_root, 'text_c10')
 manifest_file = os.path.join(data_root, 'manifest.txt')
 data_loader = BirdsDataLoader(image_dir, text_dir, manifest_file)
-iterations = 10
 
+
+
+iterations = 1
 ite = 0
 while ite < iterations:
     data = next(iter(data_loader))
     print(data[2][2].shape)
     ite += 1
+
+net = HybridCNN.TextEncoder(alphasize = 71, emb_dim = 0)
+d = torch.FloatTensor(data[:][2])
+
+net(d).shape
+
 """
 for item in data_loader:
     print(item)
